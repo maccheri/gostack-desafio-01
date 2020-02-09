40fe96030b70658182df6a884fe76572948ee02d
Felipe Maccheri <felipemaccheri@gmail.com>
2020-02-09 17:55:17 -0300

    Creating all project routes

--- index.js
+++ index.js
@@ -2,8 +2,57 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 
+const projects = [{ id: '1', title: 'teste', tasks: [] }];
 
+app.get('/projects', (req, res) => {
+  return res.json(projects);
+});
 
+app.post('/projects', (req, res) => {
+  const { id, title } = req.body;
 
-app.listen(3000);
+  projects.push({
+    id,
+    title,
+  });
+
+  return res.send('Projeto adicionado com sucesso');
+});
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const selectedProject = projects.find((project) => project.id === id);
+  if (selectedProject) {
+    selectedProject.title = title;
+
+    res.json(selectedProject);
+  }
+  res.status(500).send(`Projeto com id ${id} não encontrado`);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
+  return res.send('Projeto deletado com sucesso!');
+});
+
+app.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const selectedProject = projects.find((project) => project.id === id);
+  if (selectedProject) {
+    selectedProject.tasks.push(title);
+
+    res.json(selectedProject);
+  }
+  res.status(500).send(`Projeto com id ${id} não encontrado`);
+});
+
+app.listen(3001);
